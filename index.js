6af29f7da79decd4ea7d0d11819d25d1fda44424
Samarth Saxena <samarthsaxena1672003@gmail.com>
2023-01-12 19:18:47 +0530

    checking

--- index.js
+++ index.js
@@ -2,31 +2,55 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
+let bodyParser = require("body-parser");
+
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+// your first API endpoint...
+app.get("/api/:hello", function (req, res) {
+  let obj
+  if (req.params.hello.includes("-")) {
+    let newdate = new Date(req.params.hello);
+    console.log(newdate.toUTCString());
+    if (newdate.toUTCString() == "Invalid Date") {
+      obj = { error: newdate.toUTCString() };
+    } else {
+      obj = {utc:newdate.toUTCString(), unix:Date.parse(newdate)}
+    }
+    res.json(obj)
+  }
+  else{
+    let newdate=new Date(Number(req.params.hello))
+    console.log("pohocha")
+    console.log(newdate.toUTCString())
+    console.log(newdate.toISOString())
+    res.json({utc:newdate.toUTCString(), unix:Number(req.params.hello)})
+  }
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/", function (req, res, next) {
+  let date = new Date();
+  let strdate = date.toUTCString();
+  res.json({ unix: date.getTime(), utc: strdate });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
---
+++ tempCodeRunnerFile.javascript
@@ -0,0 +1,5 @@
+let date = new Date("1451001600000")
+let newvar =(date.toISOString())
+let anothervar = newvar.slice(0,10)
+console.log(Date.parse(date))
+
